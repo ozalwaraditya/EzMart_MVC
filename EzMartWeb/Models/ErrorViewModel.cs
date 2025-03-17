9d8ab4b90b8b6c7283bcd34b98aa14cfcf74123e
Ozalwar Aditya <adityaozalwar03@gmail.com>
2025-03-17 10:03:30 +0530

    Add project files.

---
+++ EzMartWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EzMartWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
