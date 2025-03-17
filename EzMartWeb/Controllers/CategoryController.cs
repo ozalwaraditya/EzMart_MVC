a52668121f0a07d60ccb0e41f025bf1ea903fa29
Ozalwar Aditya <adityaozalwar03@gmail.com>
2025-03-17 18:27:29 +0530

    Made initial setup, added database connection, till category objection creation in database

---
+++ EzMartWeb/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using EzMartWeb.Data;
+using EzMartWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EzMartWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList() ;
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
