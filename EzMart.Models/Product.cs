cf99a3f9e23263a918456bb353ea0718cbe58300
Ozalwar Aditya <adityaozalwar03@gmail.com>
2025-03-27 11:07:47 +0530

    Added Product controller with views

---
+++ EzMart.Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzMart.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1,1000)]
+        public double Price { get; set; }
+        
+        [Required]
+        [Range(1, 1000)]
+        [Display(Name = "Price for 50+")]
+        public double Price50 { get; set; }
+        
+        [Required]
+        [Range(1, 1000)]
+        [Display(Name = "Price for 100+")]
+        public double Price100 { get; set; }
+    }
+}
---
+++ EzMart.Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using EzMart.DataAccess.Data;
+using EzMart.Models;
+using EzMart.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EzMart.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Product product)
+        {
+            _context.Update(product);
+        }
+    }
+}
